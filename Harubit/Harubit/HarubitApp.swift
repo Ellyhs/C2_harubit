0092b11c001e5c40f2cc54b36fa62348a1924bdf
ellllly <ellllly_@10-141-48-149.0200.pos.kr.iosda.org>
2025-04-15 15:53:59 +0900

    프로젝트 생성

---
+++ Harubit/Harubit/HarubitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarubitApp.swift
+//  Harubit
+//
+//  Created by ellllly on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HarubitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
